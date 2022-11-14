4fca8b1f03e206a55a753560d904b7a21829acb8
Dario Github <dario.github@wp.pl>
2022-11-14 22:48:06 +0100

    Added simple class to check if new test case working

--- hello/project/gtestCheck.cpp
+++ hello/project/gtestCheck.cpp
@@ -3,6 +3,15 @@
 #include <iostream>
 using namespace std;
 
+class UniversityDB
+{
+    public:
+    void print()
+    {
+        std::cout << " nic " << std::endl;
+    }
+};
+
 int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
@@ -11,3 +20,9 @@ int main(int argc, char* argv[]) {
 TEST(SimpleTest, CompareEqualValues) {
     ASSERT_EQ(1, 1);
 }
+
+TEST(UniversityDB, BasicCheck) {
+    UniversityDB universityDb;
+    universityDb.print();
+}
+
