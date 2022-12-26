d1b962f9fc5d5771d44a6c0f6cabaf2d40a51100
Dario Github <dario.github@wp.pl>
2022-12-26 01:19:02 +0100

    Added snippet to load data from JSON file

--- hello/project/my_json_experiments.cpp
+++ hello/project/my_json_experiments.cpp
@@ -80,10 +80,73 @@ void example1_save_string_data_to_file()
     }
 }
 
+struct Record
+{
+    std::string name;
+    std::string surname;
+    std::string address;
+    std::string id;
+    std::string pesel;
+    std::string gender;
+};
+
+void example2_load_json_data_from_file_to_container()
+{
+    std::vector<Record> records;
+    std::ifstream in_file("data.json");
+    if (in_file.is_open())
+    {
+        // Odczytaj dane z pliku do zmiennej typu std::string
+        std::string data((std::istreambuf_iterator<char>(in_file)),
+        std::istreambuf_iterator<char>());
+        // Zamknij plik
+        in_file.close();
+
+        // Użyj biblioteki nlohmann::json do przetworzenia danych z formacie JSON na obiekt JSON
+        nlohmann::json json_data = nlohmann::json::parse(data);
+
+        // Odczytaj liczbę rekordów z obiektu JSON
+        int num_records = json_data["Number of records"];
+
+        // Utwórz wektor do przechowywania rekordów
+        
+        records.reserve(num_records);
+
+        // Iteruj przez rekordy w obiekcie JSON i dodaj je do wektora
+        for (const auto& json_record : json_data["Records"])
+        {
+            Record record;
+            record.name = json_record["Imię"];
+            record.surname = json_record["Nazwisko"];
+            record.address = json_record["Adres"];
+            record.id = json_record["Numer ID"];
+            record.pesel = json_record["Pesel"];
+            record.gender = json_record["Płeć"];
+            records.push_back(record);
+        }
+    }
+    else
+    {
+        std::cout << "Nie otworzono pliku. Koniec\n";
+        return; 
+    }
+
+    std::cout << "Wczytane dane:" << std::endl;
+    std::cout << "BazaDanychDB" << std::endl;
+    std::cout << "Number of records: " << records.size() << std::endl;
+    for (const auto& record : records)
+    {
+        std::cout << record.name << ", " << record.surname << ", "
+                << record.address << ", " << record.id << ", "
+                << record.pesel << ", " << record.gender << std::endl;
+    
+    }
+}
+
 int main() {
 
     example1_save_string_data_to_file();
-
+    example2_load_json_data_from_file_to_container();
 
 
     return 0;
